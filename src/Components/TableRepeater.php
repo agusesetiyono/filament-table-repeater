3dd698e7bc21d2fe228a8635d98f70c361375300
Agus Eko Setiyono <agusesetiyono@gmail.com>
2024-12-19 09:29:18 +0700

    don't use relationship on form filled

--- src/Components/TableRepeater.php
+++ src/Components/TableRepeater.php
@@ -5,6 +5,9 @@ namespace Awcodes\TableRepeater\Components;
 use Closure;
 use Filament\Forms\Components\Placeholder;
 use Filament\Forms\Components\Repeater;
+use Filament\Forms\Contracts\HasForms;
+use Illuminate\Database\Eloquent\Model;
+
 
 class TableRepeater extends Repeater
 {
@@ -73,5 +76,113 @@ class TableRepeater extends Repeater
     public function getView(): string
     {
         return 'table-repeater::components.table-repeater';
+    }
+
+
+    public function relationship(string | Closure | null $name = null, ?Closure $modifyQueryUsing = null): static
+    {
+        $this->relationship = $name ?? $this->getName();
+        $this->modifyRelationshipQueryUsing = $modifyQueryUsing;
+
+        // // don't use relationship on form filled
+        // $this->afterStateHydrated(function (Repeater $component) {
+        //     if (! is_array($component->hydratedDefaultState)) {
+        //         return;
+        //     }
+
+        //     $component->mergeHydratedDefaultStateWithChildComponentContainerState();
+        // });
+
+        $this->loadStateFromRelationshipsUsing(static function (Repeater $component) {
+            $component->clearCachedExistingRecords();
+
+            $component->fillFromRelationship();
+        });
+
+        $this->saveRelationshipsUsing(static function (Repeater $component, HasForms $livewire, ?array $state) {
+            if (! is_array($state)) {
+                $state = [];
+            }
+
+            $relationship = $component->getRelationship();
+
+            $existingRecords = $component->getCachedExistingRecords();
+
+            $recordsToDelete = [];
+
+            foreach ($existingRecords->pluck($relationship->getRelated()->getKeyName()) as $keyToCheckForDeletion) {
+                if (array_key_exists("record-{$keyToCheckForDeletion}", $state)) {
+                    continue;
+                }
+
+                $recordsToDelete[] = $keyToCheckForDeletion;
+                $existingRecords->forget("record-{$keyToCheckForDeletion}");
+            }
+
+            $relationship
+                ->whereKey($recordsToDelete)
+                ->get()
+                ->each(static fn(Model $record) => $record->delete());
+
+            $childComponentContainers = $component->getChildComponentContainers(
+                withHidden: $component->shouldSaveRelationshipsWhenHidden(),
+            );
+
+            $itemOrder = 1;
+            $orderColumn = $component->getOrderColumn();
+
+            $translatableContentDriver = $livewire->makeFilamentTranslatableContentDriver();
+
+            foreach ($childComponentContainers as $itemKey => $item) {
+                $itemData = $item->getState(shouldCallHooksBefore: false);
+
+                if ($orderColumn) {
+                    $itemData[$orderColumn] = $itemOrder;
+
+                    $itemOrder++;
+                }
+
+                if ($record = ($existingRecords[$itemKey] ?? null)) {
+                    $itemData = $component->mutateRelationshipDataBeforeSave($itemData, record: $record);
+
+                    if ($itemData === null) {
+                        continue;
+                    }
+
+                    $translatableContentDriver ?
+                        $translatableContentDriver->updateRecord($record, $itemData) :
+                        $record->fill($itemData)->save();
+
+                    continue;
+                }
+
+                $relatedModel = $component->getRelatedModel();
+
+                $itemData = $component->mutateRelationshipDataBeforeCreate($itemData);
+
+                if ($itemData === null) {
+                    continue;
+                }
+
+                if ($translatableContentDriver) {
+                    $record = $translatableContentDriver->makeRecord($relatedModel, $itemData);
+                } else {
+                    $record = new $relatedModel;
+                    $record->fill($itemData);
+                }
+
+                $record = $relationship->save($record);
+                $item->model($record)->saveRelationships();
+                $existingRecords->push($record);
+            }
+
+            $component->getRecord()->setRelation($component->getRelationshipName(), $existingRecords);
+        });
+
+        $this->dehydrated(false);
+
+        $this->disableItemMovement();
+
+        return $this;
     }
 }
